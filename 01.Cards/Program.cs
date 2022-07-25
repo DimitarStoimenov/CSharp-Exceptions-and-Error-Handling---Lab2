a1c8c500121dc076132ee48d30446b7a6e549b97
DimitarStoimenov <stoimenovdimitur@yahoo.com>
2022-07-25 23:34:43 +0200

    C#-OOP

---
+++ 01.Cards/Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cards
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+            string[] input = Console.ReadLine().Split(", ");
+            List<Card> cards = new List<Card>();
+            Card card = null;
+            for (int i = 0; i < input.Length; i++)
+            {
+
+                string[] cardinfo = input[i].Split();
+                string face = cardinfo[0].ToUpper();
+                string suite = cardinfo[1].ToUpper();
+
+                try
+                {
+
+                   
+                    
+                    CreateCard(face, suite);
+                    if (suite == "S")
+                    {
+                        string s = "\u2660";
+                        byte[] bytes = Encoding.Default.GetBytes(s);
+                        s = Encoding.UTF8.GetString(bytes);
+
+                        suite = s;
+
+                    }
+                    if (suite == "H")
+                    {
+                        string h = "\u2665";
+                        byte[] bytes = Encoding.Default.GetBytes(h);
+                        h = Encoding.UTF8.GetString(bytes);
+
+                        suite = h;
+
+                    }
+                    if (suite == "D")
+                    {
+                        string d = "\u2666";
+                        byte[] bytes = Encoding.Default.GetBytes(d);
+                        d = Encoding.UTF8.GetString(bytes);
+
+                        suite = d;
+
+                    }
+                    if (suite == "C")
+                    {
+                        string c = "\u2663";
+                        byte[] bytes = Encoding.Default.GetBytes(c);
+                        c = Encoding.UTF8.GetString(bytes);
+
+                        suite = c;
+
+                    }
+
+                    card = new Card(face, suite);
+                    
+                    cards.Add(card);
+                }
+                catch (Exception ae)
+                {
+
+                    Console.WriteLine(ae.Message);
+                }
+               
+
+            }
+
+            foreach (var item in cards)
+            {
+                Console.Write(item.ToString());
+            }
+
+        }
+        public static void CreateCard(string face, string suite)
+        {
+            
+
+            if (face != "2" && face != "3" && face != "4" && face != "5" && face != "6" && face != "7" && face != "8" && face != "9" && face != "10" && face != "J" && face != "A" && face != "K" && face != "Q")
+            {
+                throw new Exception("Invalid card!");
+            }
+            if ((suite != "S") && (suite != "H") && (suite != "D") && (suite != "C"))
+            {
+                throw new Exception("Invalid card!");
+            }
+
+        }
+    }
+
+    public class Card
+    {
+        
+        public Card(string face, string suite)
+        {
+            this.Face = face;
+            this.Suite = suite;
+        }
+
+        public string  Face { get; set; }
+        public string  Suite { get; set; }
+
+        public override string ToString()
+        {
+            return $"[{this.Face}{this.Suite}]" + " ";
+        }
+    }
+}
---
+++ 02.SumofIntegers/Program.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SumofIntegers
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            List<int> print = new List<int>();
+            int exceptions = 0;
+            while (exceptions != 3)
+            {
+                string[] command = Console.ReadLine().Split();
+                string typeCommand = command[0];
+                try
+                {
+                    if (typeCommand == "Replace")
+                    {
+                        IsIndexOutOf(input, command[1]);
+                        int index = int.Parse(command[1]);
+                        int element = int.Parse(command[2]);
+
+                        input[index] = element;
+                    }
+                    else if (typeCommand == "Print")
+                    {
+                        IsPrintAvalable(input, command[1], command[2]);
+                        int startIndex = int.Parse(command[1]);
+                        int endIndex = int.Parse(command[2]);
+                        for (int i = input[startIndex] - 1; i <= endIndex; i++)
+                        {
+                            print.Add(input[i]);
+                        }
+
+                        Console.WriteLine(string.Join(", ", print));
+                        print.Clear();
+
+                    }
+                    else if (typeCommand == "Show")
+                    {
+                        IsIndexOutOf(input, command[1]);
+                        int index = int.Parse(command[1]);
+                        Console.WriteLine(input[index]);
+
+                    }
+
+                }
+                catch (InvalidIndex ii)
+                {
+                    exceptions++;
+                    Console.WriteLine(ii.Message);
+
+                }
+
+                catch (InvalidVariable iv)
+                {
+                    exceptions++;
+                    Console.WriteLine(iv.Message);
+
+                }
+
+            }
+
+            Console.WriteLine(string.Join(", ", input));
+
+        }
+        public static void IsIndexOutOf(int[] input, string index)
+        {
+            int number;
+            bool success = int.TryParse(index, out number);
+           
+
+            if (!success)
+            {
+                throw new InvalidVariable("The variable is not in the correct format!");
+            }
+            if (number < 0 || number > input.Length - 1)
+            {
+                
+                throw new InvalidIndex("The index does not exist!");
+            }
+           
+           
+        }
+        public static void IsPrintAvalable(int[] input, string index, string input2)
+        {
+            int number;
+            bool success = int.TryParse(index, out number);
+            int number2;
+            bool success2 = int.TryParse(input2, out number2);
+
+
+            if (!success || !success2)
+            {
+                throw new InvalidVariable("The variable is not in the correct format!");
+            }
+            if (number < 0 || number2 > input.Length - 1)
+            {
+
+                throw new InvalidIndex("The index does not exist!");
+            }
+
+
+        }
+
+
+    }
+    public class InvalidIndex : Exception
+    {
+        public InvalidIndex(string message)
+            : base(message)
+        {
+
+        }
+    }
+    public class InvalidVariable : Exception
+    {
+        public InvalidVariable(string message)
+            : base(message)
+        {
+
+        }
+    }
+
+}
+
+
+
